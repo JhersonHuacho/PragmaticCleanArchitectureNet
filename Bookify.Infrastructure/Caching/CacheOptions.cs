a3b05815dea357d44b5a33e91c1bc63a05cf2041
Francisco <jhersonhuacho@gmail.com>
2025-01-04 11:15:52 -0500

    02 configuration caching with redis and implementing

---
+++ Bookify.Infrastructure/Caching/CacheOptions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace Bookify.Infrastructure.Caching
+{
+	public static class CacheOptions
+	{
+		public static DistributedCacheEntryOptions DefaultExpiration => new DistributedCacheEntryOptions
+		{
+			AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1)
+		};
+
+		public static DistributedCacheEntryOptions Create(TimeSpan? expiration)
+		{
+			return expiration is not null
+				? new DistributedCacheEntryOptions { AbsoluteExpirationRelativeToNow = expiration }
+				: DefaultExpiration;
+		}
+	}
+}
