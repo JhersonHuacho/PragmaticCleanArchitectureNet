dbabe979fc145f5f473f0532ea9b69da5b4a708c
Francisco <jhersonhuacho@gmail.com>
2025-01-05 11:22:52 -0500

    01-05 fix to model binder and migration bd and validation api

--- Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
+++ Bookify.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -6,6 +6,8 @@ namespace Bookify.Api.Extensions
 {
 	public static class ApplicationBuilderExtensions
 	{
+		// this method ApplyMigration is used to apply the migration to the database for local development purposes
+		// to take the application builder and create a scope, use this scope to resolve my database context and the apply any pending migrations to my database
 		public static void ApplyMigration(this IApplicationBuilder app) 
 		{
 			using var scope = app.ApplicationServices.CreateScope();
