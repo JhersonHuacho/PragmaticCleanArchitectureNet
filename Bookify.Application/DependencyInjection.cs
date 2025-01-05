9279a102e010f4210faba0ced367a0fe0288f33d
Francisco <jhersonhuacho@gmail.com>
2025-01-04 20:25:56 -0500

    02 Implementing a generic caching solution for our mediator queries

--- Bookify.Application/DependencyInjection.cs
+++ Bookify.Application/DependencyInjection.cs
@@ -12,10 +12,14 @@ namespace Bookify.Application
 			services.AddMediatR(configuration => 
 			{
 				configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+				
 				configuration.AddOpenBehavior(typeof(LoggingBehaviors<,>));
+				
 				#region Add open behavior for validation
 				configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
 				#endregion
+
+				configuration.AddOpenBehavior(typeof(QueryCachingBehavior<,>));
 			});
 
 			#region Add validators from assembly for validation. Using reflection to get all validators from the assembly.
