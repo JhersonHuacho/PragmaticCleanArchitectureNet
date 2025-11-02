1f7e19e049d0b7041a318df1ba5713a07d09f95a
Francisco <jhersonhuacho@gmail.com>
2025-11-01 22:37:37 -0500

    Application Layer Unit Testing

---
+++ test/Bookify.Application.UnitTests/Apartments/ApartmentData.cs
@@ -0,0 +1,21 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Shared;
+
+namespace Bookify.Application.UnitTests.Apartments
+{
+	internal static class ApartmentData
+	{
+		public static Apartment Create()
+		{
+			return new Apartment(
+				Guid.NewGuid(),
+				new Name("Test Apartment"),
+				new Description("Test Description."),
+				new Address("Country", "State", "ZipCode", "City", "Street"),
+				new Money(100.0m, Currency.Usd),
+				Money.Zero(),
+				new List<Amenity>()
+			);
+		}
+	}
+}
