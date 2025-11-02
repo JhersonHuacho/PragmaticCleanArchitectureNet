33024cdf5299d88393d7dd0b671ab70507eba656
Francisco <jhersonhuacho@gmail.com>
2025-11-01 21:28:26 -0500

    Domain Layer Unit Testing

---
+++ test/Bookify.Domain.UnitTests/Infrastructure/BaseTest.cs
@@ -0,0 +1,20 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.UnitTests.Infrastructure
+{
+	public abstract class BaseTest
+	{
+		public static T AssertDomainEventWasPublished<T>(Entity entity)
+			where T : IDomainEvent
+		{
+			var domainEvent = entity.GetDomainEvents().OfType<T>().FirstOrDefault();
+			
+			if (domainEvent == null)
+			{
+				throw new Exception($"Expected domain event of type {typeof(T).Name} was not published.");
+			}
+
+			return domainEvent;
+		}
+	}
+}
